d2a4eadb1fff7c84b83c06b97da19a420712c831
Pascoal Eddy Bayonne <eddybayonne@gmail.com>
2018-10-07 23:09:15 +0100

    Transformer adaptor (endpoint)

---
+++ SI-Transformer-PT-II/src/main/java/com/pascoal/Main.java
@@ -0,0 +1,11 @@
+package com.pascoal;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext
+                = new ClassPathXmlApplicationContext("META-INF/spring/si-components.xml");
+    }
+}
---
+++ SI-Transformers/README.txt
@@ -0,0 +1,3 @@
+    Transformers take a message from a channel and creates a new message containing converted payload or message structure.
+This allows the provider of information and the consumer of information to communicate via SI without having to settle on
+a common format. XML can be transformed to JSON, JSON transformed to Java objects, etc.
